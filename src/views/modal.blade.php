36749ff9f526b0cda06d0d26816ce071e8d94dbf
devsrv <developer.srv1@gmail.com>
2018-10-21 21:33:01 +0530

    modal is seperated for user config

---
+++ src/views/modal.blade.php
@@ -0,0 +1,9 @@
+<div id="modal-devsrv">
+    <label class="modal-devsrv__bg" onclick="closeSessionOutModal();"></label>
+    <div class="modal-devsrv__inner">
+        <label class="modal-devsrv__close" onclick="closeSessionOutModal();"></label>
+        <h2>SESSION EXPIRED!</h2>
+        <p>Your session has expired,</p>
+        <p>to resume your current activity please login again in another tab</p>
+    </div>
+</div>
--- src/views/notify.blade.php
+++ src/views/notify.blade.php
@@ -2,15 +2,7 @@
     <link rel="stylesheet" href="{{ asset('vendor/sessionout/css/session-modal.css') }}" />
 
     {{-- Modal --}}
-    <div id="modal-devsrv">
-        <label class="modal-devsrv__bg" onclick="closeSessionOutModal();"></label>
-        <div class="modal-devsrv__inner">
-            <label class="modal-devsrv__close" onclick="closeSessionOutModal();"></label>
-            <h2>SESSION EXPIRED!</h2>
-            <p>Your session has expired,</p>
-            <p>to resume your current activity please login again in another tab</p>
-        </div>
-    </div>
+    @include('vendor.sessionout.modal')
 
 <script type="text/javascript">
     window.sessionout = window.sessionout || {};
