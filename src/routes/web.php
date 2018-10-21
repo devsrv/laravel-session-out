e40e5f167e32acaeb13c913d908d2f38ba76ce83
devsrv <developer.srv1@gmail.com>
2018-10-21 23:49:06 +0530

    config file data fetch

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'devsrv\sessionout\Http\Controllers', 'middleware' => 'web'], function () {
+Route::group(['namespace' => 'devsrv\sessionout\Http\Controllers', 'middleware' => config('expiredsession.route.middleware')], function () {
     Route::post('/check-auth', 'AuthCheckCtrl@getStatus')->name('sessionout.chkauth');
 });
--- src/views/notify.blade.php
+++ src/views/notify.blade.php
@@ -8,6 +8,7 @@
     window.sessionout = window.sessionout || {};
     sessionout.authStatus = 1;
     sessionout.authpingEndpoint = "{{ route('sessionout.chkauth') }}";
+    sessionout.requestGap = {{ config('expiredsession.gap_seconds') }};
 </script>
 <script type="text/javascript" src="{{ asset('vendor/sessionout/dist/js/main.js') }}"></script>
 <script type="text/javascript">
